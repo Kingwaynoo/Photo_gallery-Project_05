99f2857734caf3d3e55579d60f44d5d7c9ab4505
wayne <wayne.smith@ignitiongroup.co.za>
2022-01-29 14:02:12 +0200

    Photo_gallery Project_05

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+baguetteBox.run('#gallery');
+
+let userInput = document.querySelector('.search');
+let a = document.querySelectorAll('a');
+let imageArray = Array.from(a);
+
+const searchFilter = () => {
+  for (i=0; i<imageArray.length; i++){
+    let dataCaption = imageArray[i].getAttribute('data-caption').toLowerCase();
+
+
+    if (dataCaption.includes(userInput.value.toLowerCase())){
+      imageArray[i].style.display = 'block';
+    } else {
+      imageArray[i].style.display = 'none';
+    }
+  }
+};
